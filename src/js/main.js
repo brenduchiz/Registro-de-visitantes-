3af159f70aff229af42d7ebaf94fd1ae9b30fc6e
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-09 16:01:00 -0500

    Se agrego funcionalidad para mostrar la data

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 document.getElementById('camera').addEventListener('click', event => {
   document.getElementById('photo').innerHTML = `<video id="video"></video>
     <br>
-    <button id="startbutton">Tomar fotografia</button>
+    <button class="btn btn-info" id="startbutton">Tomar fotografia</button>
     <br>
     <canvas id="canvas"></canvas>`;
   window.client.photo();
+});
+
+document.getElementById('boton').addEventListener('click', event => {
+  window.admin.showTable();
 });
