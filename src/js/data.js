26c6d1a9a8799f38c0c348544504266bae16132d
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-09 15:59:11 -0500

    Separando funcionabilidad en cliente y administrador

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,41 @@
+firebase.initializeApp({
+  apiKey: 'AIzaSyBgC8VSVMtFdRtd0fGVxQVTG_DQskngoUw',
+  authDomain: 'registrovisitantes-8986e.firebaseapp.com',
+  projectId: 'registrovisitantes-8986e'
+});
+
+// Initialize Cloud Firestore through Firebase
+let db = firebase.firestore();
+
 window.client = {
-  register: () => { },
+  save: () => {
+    // Agregar docuemento
+    let email = document.getElementById('email').value;
+    let nombre = document.getElementById('nombre').value;
+    let password = document.getElementById('password').value;
+    let empresa = document.getElementById('empresa').value;
+    let direccion = document.getElementById('direccion').value;     
+
+    db.collection('users').add({
+      email: email,
+      nombre: nombre,
+      password: password,
+      empresa: empresa,
+      direccion: direccion
+    })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+        document.getElementById('email').value = '';
+        document.getElementById('nombre').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('empresa').value = '';
+        document.getElementById('direccion').value = '';
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+  },
+
   photo: () => {
     (() => {
       let streaming = false,
@@ -49,6 +85,7 @@ window.client = {
         canvas.height = height;
         canvas.getContext('2d').drawImage(video, 0, 0, width, height);
         let finalPhoto = canvas.toDataURL('image/png');
+        video.stop();
         return finalPhoto;
       };
 
@@ -62,5 +99,76 @@ window.client = {
 };
 
 window.admin = {
-  stats: () => { }
+  // borrar documentos
+  showTable: () => {
+    // Leer documentos
+    let tabla = document.getElementById('tabla');
+    db.collection('users').onSnapshot((querySnapshot) => {
+      tabla.innerHTML = '';
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().email}`);
+        tabla.innerHTML += `
+          <tr>
+          <th scope="row">${doc.id}</th>
+          <td>${doc.data().email}</td>
+          <td>${doc.data().nombre}</td>
+          <td>${doc.data().password}</td>
+          <td>${doc.data().empresa}</td>
+          <td>${doc.data().direccion}</td>
+          <td><button class="btn btn-danger" onclick="window.admin.delete('${doc.id}')">Eliminar</button></td>
+          <td><button class="btn btn-warning" onclick="window.admin.edit('${doc.id}','${doc.data().email}','${doc.data().nombre}','${doc.data().password}','${doc.data().empresa}','${doc.data().dirección}')">Editar</button></td>
+        </tr>`;
+      });
+    });
+  },
+
+  delete: (id) => {
+    db.collection('users').doc(id).delete().then(() => {
+      console.log('Document successfully deleted!');
+    }).catch((error) => {
+      console.error('Error removing document: ', error);
+    });
+  },
+
+  // editar documentos
+  edit: (id, email, nombre, password, empresa, direccion) => {
+    document.getElementById('email').value = email;
+    document.getElementById('nombre').value = nombre;
+    document.getElementById('password').value = password;
+    document.getElementById('empresa').value = password;
+    document.getElementById('direccion').value = password;
+    let boton = document.getElementById('boton');
+    boton.innerHTML = 'Editar';
+
+    boton.onclick = (() => {
+      let washingtonRef = db.collection('users').doc(id);
+        
+      let email = document.getElementById('email').value;
+      let nombre = document.getElementById('nombre').value;
+      let password = document.getElementById('password').value;
+      let empresa = document.getElementById('empresa').value;
+      let direccion = document.getElementById('direccion').value;
+
+      return washingtonRef.update({
+        email: email,
+        nombre: nombre,
+        password: password,
+        empresa: empresa,
+        direccion: direccion
+      })
+        .then(() => {
+          console.log('Document successfully updated!');
+          boton.innerHTML = 'Guardar';
+          document.getElementById('email').value = '';
+          document.getElementById('nombre').value = '';
+          document.getElementById('password').value = '';
+          document.getElementById('empresa').value = '';
+          document.getElementById('direccion').value = '';
+        })
+        .catch((error) => {
+          // The document probably doesn't exist.
+          console.error('Error updating document: ', error);
+        });
+    });
+  }
 };
