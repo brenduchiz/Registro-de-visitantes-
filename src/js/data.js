617bfe6d5e712137d5606c0bb24ce282a8fb5301
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-09 09:55:56 -0500

    Se agrego la ventana de camara y el boton que toma la foto

--- src/js/data.js
+++ src/js/data.js
@@ -1,7 +1,67 @@
 window.client = {
-  register: () => {}
+  register: () => { },
+  photo: () => {
+    (() => {
+      let streaming = false,
+        video = document.querySelector('#video'),
+        canvas = document.querySelector('#canvas'),
+        startbutton = document.querySelector('#startbutton'),
+        width = 320,
+        height = 0;
+
+      navigator.getMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+      navigator.getMedia(
+        {
+          video: true,
+          audio: false
+        },
+        (stream) => {
+          if (navigator.mozGetUserMedia) {
+            video.mozSrcObject = stream;
+          } else {
+            let vendorURL = window.URL || window.webkitURL;
+            video.src = vendorURL.createObjectURL(stream);
+          }
+          video.play();
+        },
+        (err) => {
+          console.log('An error occured! ' + err);
+        }
+      );
+
+      video.addEventListener('canplay', event => {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+          video.setAttribute('width', width);
+          video.setAttribute('height', height);
+          canvas.setAttribute('width', width);
+          canvas.setAttribute('height', height);
+          streaming = true;
+        }
+      }, false);
+
+      const takepicture = () => {
+        canvas.width = width;
+        canvas.height = height;
+        canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+        let photo = canvas.toDataURL('image/png');
+      };
+
+      startbutton.addEventListener('click', event => {
+        takepicture();
+        event.preventDefault();
+      }, false);
+    })
+    ();
+  }
 };
 
 window.admin = {
-  stats: () => {}
+  stats: () => { }
 };
+
+window.client.photo();
