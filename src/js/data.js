e50e8882d8a80c1cc6c672e906f88656eb9352e4
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-13 18:54:57 -0500

    modificando para hacer funcional la camara

--- src/js/data.js
+++ src/js/data.js
@@ -1,57 +1,6 @@
 window.client = {
-  guardar: () => {
-    let email = document.getElementById('email').value;
-    let nombre = document.getElementById('nombre').value;
-    let empresa = document.getElementById('empresa').value;
-    let visitados = document.getElementById('visitados').value;
-    let motivo = document.getElementById('motivo').value;
-
-    db.collection('users').add({
-      nombre: nombre,
-      email: email,
-      empresa: empresa,
-      visitado: visitados,
-      motivo: motivo
-    })
-      .then((docRef) => {
-        console.log('Document written with ID: ', docRef.id);
-        document.getElementById('email').value = '';
-        document.getElementById('nombre').value = '';
-        document.getElementById('empresa').value = '';
-        document.getElementById('visitado').value = '';
-        document.getElementById('motivo').value = '';
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      });
-
-
-    // Leer documentos
-    let delate = document.getElementById('delate');
-    db.collection('users').onSnapshot((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().email}`);
-        console.log(doc.data());
-        console.log(nombre);
-        delate.innerHTML = `
-            <div class="row">            
-            <div class="card" style="width: auto; margin: 100px auto;">
-            <div class="card-body">
-            <h5 class="card-title">Bienvenidx a Terminal 1</h5>
-              <h6 class="card-title">${doc.data().nombre}</h6>
-              <p class="card-text"><b>Correo:</b>  ${doc.data().email}</p>
-              <p class="card-text"><b>Empresa:</b>  ${doc.data().empresa}</p>
-              <p class="card-text"><b>Visita a :</b>  ${doc.data().visitado}</p>
-              <p class="card-text"><b>Motivo de visita:</b>  ${doc.data().motivo}</p>
-              <a href="#" class="btn btn-raised btn-raised" onclick="edit();">Editar Campos</a>
-              <a href="#" class="btn btn-raised btn-raised">Finalizar Registro</a>
-            </div>
-          </div>
-          </div>`;
-      });
-    });
-  },
   photo: () => {
+    let storage = firebase.storage();
     (() => {
       let streaming = false,
         video = document.querySelector('#video'),
@@ -99,7 +48,20 @@ window.client = {
         canvas.width = width;
         canvas.height = height;
         canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+        document.getElementById('video').style.display = 'none';
+        document.getElementById('startbutton').style.display = 'none';
         let finalPhoto = canvas.toDataURL('image/png');
+        localStorage.setItem('userPhoto', finalPhoto);
+        let storageRef = storage.ref('visitor-images/');
+        console.log(storageRef);
+        const newVisitorKey = firebase.database().ref().child('visitors').push().key;
+        let images = storageRef.child(newVisitorKey);
+        sessionStorage.setItem('visitorKey', newVisitorKey);
+
+
+        images.putString(finalPhoto, 'data_url').then((snapshot) => {
+          console.log('Uploaded a data_url string!');
+        });
       };
 
       startbutton.addEventListener('click', event => {
@@ -108,6 +70,69 @@ window.client = {
       }, false);
     })
     ();
+  },
+  guardar: () => {
+    let email = document.getElementById('email').value;
+    let nombre = document.getElementById('nombre').value;
+    let empresa = document.getElementById('empresa').value;
+    let visitados = document.getElementById('visitados').value;
+    let motivo = document.getElementById('motivo').value;
+    let photito = localStorage.getItem('userPhoto');
+    let newDates = new Date();
+    let time = newDates.toLocaleTimeString();
+    let date = newDates.toLocaleDateString();
+
+    db.collection('users').add({
+      time: time,
+      date: date,
+      nombre: nombre,
+      email: email,
+      empresa: empresa,
+      visitado: visitados,
+      motivo: motivo,
+      photo: photito
+    })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+        document.getElementById('email').value = '';
+        document.getElementById('nombre').value = '';
+        document.getElementById('empresa').value = '';
+        document.getElementById('visitado').value = '';
+        document.getElementById('motivo').value = '';
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+
+
+    // Leer documentos
+    let delate = document.getElementById('delate');
+    db.collection('users').onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id}`);
+        console.log(doc.data());
+        console.log(nombre);
+        let saveId = doc.id;
+        console.log(saveId);        
+        let savePhotito = doc.data().photo;        
+        delate.innerHTML = `
+            <div class="row">            
+            <div class="card" style="width: auto; margin: 100px auto;">
+            <div class="card-body">
+            <h5 class="card-title">Bienvenidx a Terminal 1</h5>
+            <img src='${savePhotito}'>
+              <h6 class="card-title">${doc.data().nombre}</h6>
+              <p class="card-text"><b>Correo:</b>  ${doc.data().email}</p>
+              <p class="card-text"><b>Empresa:</b>  ${doc.data().empresa}</p>
+              <p class="card-text"><b>Visita a :</b>  ${doc.data().visitado}</p>
+              <p class="card-text"><b>Motivo de visita:</b>  ${doc.data().motivo}</p>
+              <a href="#" class="btn btn-raised btn-raised" id="editRegister">Editar Campos</a>
+              <a href="#" class="btn btn-raised btn-raised" id="registerFinish">Finalizar Registro</a>
+            </div>
+          </div>
+          </div>`;
+      });
+    });
   },
   edit: () => {
     delate.style.display = 'none';
@@ -130,7 +155,8 @@ window.client = {
         email: email,
         empresa: empresa,
         visitado: visitados,
-        motivo: motivo
+        motivo: motivo,
+        photo: photito
       })
         .then(function() {
           console.log('Document successfully updated!');
@@ -147,8 +173,4 @@ window.client = {
         });
     };
   }
-};
-
-window.admin = {
-  stats: () => { }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,4 @@
-
-// Direccionamiento a Interfaz Registro 
+//Direccionamiento a Interfaz Registro 
 document.getElementById('colorbtn').addEventListener('click', event => {
-  window.location.assign('views/Formulario.html');
-});
-
-document.getElementById('camera').addEventListener('click', event => {
-  document.getElementById('photo').innerHTML = `<video id="video"></video>
-      <br>
-      <button id="startbutton">Tomar fotografia</button>
-      <br>
-      <canvas id="canvas"></canvas>`;
-  window.client.photo();
-});
-
-
-document.getElementById('Registrarte').addEventListener('click', event => {
-  window.client.guardar();
+  window.location.assign("views/Formulario.html");
 });
