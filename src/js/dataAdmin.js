bd62ddb248ce1f193fc2ba1739a8610858b241f3
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-14 08:52:52 -0500

    Mejorando vista administrador

--- src/js/dataAdmin.js
+++ src/js/dataAdmin.js
@@ -1,43 +1,42 @@
 firebase.initializeApp({
-    apiKey: "AIzaSyBgC8VSVMtFdRtd0fGVxQVTG_DQskngoUw",
-    authDomain: "registrovisitantes-8986e.firebaseapp.com",
-    projectId: "registrovisitantes-8986e"
-  });
+  apiKey: 'AIzaSyBgC8VSVMtFdRtd0fGVxQVTG_DQskngoUw',
+  authDomain: 'registrovisitantes-8986e.firebaseapp.com',
+  projectId: 'registrovisitantes-8986e'
+});
   
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
  
-     //Leer documentos
-     var tabla = document.getElementById("tabla");
-    db.collection("users").onSnapshot((querySnapshot) => {
-        tabla.innerHTML = "";
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data().email}`);
-            tabla.innerHTML += `
+// Leer documentos
+var tabla = document.getElementById('tabla');
+db.collection('users').onSnapshot((querySnapshot) => {
+  tabla.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data().email}`);
+    tabla.innerHTML += `
             <tr>
-            <th scope="row">${doc.id}</th>
+            <td><img src='${doc.data().photo}' height="80" width="98"></td>
     
             <td>${doc.data().email}</td>
             <td>${doc.data().nombre}</td>
             <td>${doc.data().empresa}</td>
             <td>${doc.data().motivo}</td>
             <td>${doc.data().visitado}</td>
-            <td>${doc.data().date}</td>
+            <td>${doc.data().date} ${doc.data().time}</td>
             <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
-            <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().photo}','${doc.data().email}','${doc.data().nombre}','${doc.data().empresa}','${doc.data().empresa}','${doc.data().motivo}','${doc.data().visitado}','${doc.data().date}')">Editar</button></td>
-          </tr>`
-        });
-    });
+          </tr>`;
+  });
+});
   
 
-    //borrar documentos
-    function eliminar(id){
-        db.collection("users").doc(id).delete().then(function() {
-            console.log("Document successfully deleted!");
-        }).catch(function(error) {
-            console.error("Error removing document: ", error);
-        });
-    }
+// borrar documentos
+function eliminar(id) {
+  db.collection('users').doc(id).delete().then(function() {
+    console.log('Document successfully deleted!');
+  }).catch(function(error) {
+    console.error('Error removing document: ', error);
+  });
+}
 /*
     //editar documentos
     function editar(id,email,nombre,password,empresa,direccion){
